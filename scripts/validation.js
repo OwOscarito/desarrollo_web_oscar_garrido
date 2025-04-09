1f914fc5abf5587c9d17c9352ac8b188e5158c43
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2025-04-09 00:10:27 -0400

    fix js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,66 +1,167 @@
-const validateLenght = (input, max_length) => {
-  return input.length < max_length;
+const validateLenght = (input, min_lenght, max_length) => {
+  return  min_lenght <= input.length && input.length <= max_length;
 };
 
-const validateRegion = (regionId) => {
-  foundRegion = region_comunas.regiones.find(
-    (region) =>  region.numero == regionId
-  );
-  return (foundRegion != undefined);
-}
-
-const validateCommune = (regionId, communeId) => {
-  foundRegion = region_comunas.regiones.find((region) => {
-    return region.numero == regionId;
-  });
-  foundCommune = foundRegion.comunas.find(
-    (comunne) => comunne.id == communeId);
-
-  return (foundCommune != undefined);
-}
-
-const validateSector = (sector) => {
-  const sectorRegex = /^[A-Z]{2}$/;
-  return validateLenght(sector, 2) && sectorRegex.test(sector);
-}
-
-const validateName = (name) => {
-  return validateLenght(name, 200);
-};
-
-const validateEmail = (email) => {
-  const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return validateLenght(email, 100) && emailRegex.test(email);
-};
-
-const validatePhoneNumber = (number) => {
-  const phoneRegex = /^\+\d{3}\.\d{8}$/;
-  return (validateLenght(number, 14) && phoneRegex.test(number));
-};
-
-const validateContact = (contact) => {
-  const contactRegex = /^[A-Za-z0-9\s]+$/;
-  return validateLenght(contact, );
-};
-
-const validateDate = (date) => {
-  // Regex for validating date in YYYY-MM-DD hh:mmformat
-  const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/;
-  return validateLenght(date, 10) && dateRegex.test(date);
-};
-
-const validateEndDate = (startDate, endDate) => {
-  const start = new Date(startDate);
-  const end = new Date(endDate);
-  return end > start;
-}
-
-const validateDescription = (description) => {
-  return validateLenght(description, 500);
-}
-
-const validateTopic = (topic, otherTopic) => {
-  if (topic == "Otro") {
-    return validateLenght(otherTopic, 200) ;
+const validateWhere = () => {
+  const validateRegion = (regionId) => {
+    foundRegion = region_comunas.regiones.find(
+      (region) =>  region.numero == regionId
+    );
+    return (foundRegion != undefined);
   }
+
+  const validateCommune = (regionId, communeId) => {
+    foundRegion = region_comunas.regiones.find((region) => {
+      return region.numero == regionId;
+    });
+    foundCommune = foundRegion.comunas.find(
+      (comunne) => comunne.id == communeId);
+
+    return (foundCommune != undefined);
+  }
+
+  const validateSector = (sector) => {
+    return validateLenght(sector, 0, 100);
+  }
+
+  const selectRegion = document.getElementById("select-region").value;
+  const selectCommune = document.getElementById("select-commune").value;
+  const sector = document.getElementById("sector").value;
+
+  if (!validateRegion(selectRegion)) {
+    alert("Seleccione una región válida.");
+    return false;
+  };
+
+  const selectCommuneValid = validateCommune(selectRegion, selectCommune);
+  if (!selectCommuneValid) {
+    alert("Seleccione una comuna válida.");
+    return false;
+  };
+
+  const sectorValid = validateSector(sector);
+  if (!sectorValid) {
+    alert("Ingrese un sector válido.");
+  return false;
+  };
+  return true;
+};
+
+const validateWho = () => {
+
+  const validateName = (name) => {
+    return validateLenght(name, 1, 200);
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return validateLenght(email, 1, 100) && emailRegex.test(email);
+  };
+
+  const validatePhoneNumber = (number) => {
+    const phoneRegex = /^\+\d{3}\.\d{8}$/;
+    return (validateLenght(number, 14, 14) && phoneRegex.test(number));
+  };
+
+  const validateContact = (contact) => {
+    return validateLenght(contact, 4, 50);
+  };
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("phone").value;
+  const whatsapp = document.getElementById("whatsapp").value;
+  const telegram = document.getElementById("telegram").value;
+  const instagram = document.getElementById("instagram").value;
+  const tiktok = document.getElementById("tiktok").value;
+  const x = document.getElementById("x").value;
+  const other = document.getElementById("other").value;
+
+  
+  if (!validateName(name)) {
+    alert("Ingrese un nombre válido.");
+    return false;
+  }
+
+  if (!validateEmail(email)) {
+    alert("Ingrese un correo electrónico válido.");
+    return false;
+  }
+
+  if (!validatePhoneNumber(phoneNumber)) {
+    alert("Ingrese un número de teléfono válido.");
+    return false;
+  }
+
+  if (!validateContact(whatsapp)) {
+    alert("Ingrese un contacto de WhatsApp válido.");
+    return false;
+  }
+  if (!validateContact(telegram)) {
+    alert("Ingrese un contacto de Telegram válido.");
+    return false;
+  }
+  if (!validateContact(instagram)) {
+    alert("Ingrese un contacto de Instagram válido.");
+    return false;
+  }
+  if (!validateContact(tiktok)) {
+    alert("Ingrese un contacto de TikTok válido.");
+    return false;
+  }
+  if (!validateContact(x)) {
+    alert("Ingrese un contacto de X válido.");
+    return false;
+  }
+  if (!validateContact(other)) {
+    alert("Ingrese un contacto adicional válido.");
+    return false;
+  }
+  return true;
+};
+
+const validateWhen = () => {
+  const validateDate = (date) => {
+    // Regex for validating date in YYYY-MM-DD hh:mmformat
+    const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/;
+    return validateLenght(date, 10) && dateRegex.test(date);
+  };
+
+  const validateEndDate = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    return end > start;
+  }
+
+  if (!validateDate(startDate)) {
+    alert("Ingrese una fecha de inicio válida.");
+    return false;
+  }
+  if (!validateDate(endDate)) {
+    alert("Ingrese una fecha de término válida.");
+    return false;
+  }
+  return true;
+};
+
+const validateWhat = () => {
+
+  const validateDescription = (description) => {
+    return validateLenght(description, 500);
+  }
+
+  const validateTopic = (topic, otherTopic) => {
+    if (topic == "Otro") {
+      return validateLenght(otherTopic, 200) ;
+    } else {
+      return topics.includes(topic);
+    }
+  }
+
+
+
+};
+
+const validateForm = () => {
+  return validateWhere() && validateWho() && validateWhen() && validateWhat();
 }
