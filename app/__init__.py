42cea9908f3c620048b7f6c4cf4e098f10bf6178
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2025-05-14 22:39:10 -0400

    set css template + reestructure html + set urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for
 from app.database import db
 from werkzeug.utils import secure_filename
-
+from app.utils import validate
 
 UPLOAD_FOLDER = 'static/uploads'
 
@@ -15,11 +15,47 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def base():
     return render_template('base.html')
 
+@app.route('/estadisticas',methods=["GET"])
+def estadisticas():
+    return render_template('estadisticas.html')
+
+@app.route('/agregar',methods=["GET", "POST"])
+def agregar():
+    if request.method == "POST":
+        req = request.form
+        error = ""
+        render_template('agregar-actividad.html', error=error)
+        return redirect(url_for('index'))
+                        
+    if request.method == "GET":
+        return render_template('agregar-actividad.html')
+
+    return render_template('agregar-actividad.html')
+
+@app.route('/actividad/<int:id>',methods=["GET"])
+@app.route('/actividad',methods=["GET"])
+def actividad(id=None):
+    if not id:
+        return redirect(url_for('listado'))
+    
+    actividad = db.get_activity(id)
+    if not actividad:
+        return redirect(url_for('listado'))
+    return render_template('informacion-actividad.html', actividad=actividad)
+
+@app.route('/listado',methods=["GET"])
+def listado():
+    actividades = []
+    #for actividad in db.get_activities(5):
+    #    actividades.append(actividad)
+    return render_template('listado-actividades.html', actividades=actividades)
+
 @app.route('/',methods=["GET"])
 def index():
-    data = []
-    activities = db.get_activities(5)
-    return render_template('index.html')
+    actividades = []
+    #for actividad in db.get_activities(5):
+    #    actividades.append(actividad)
+    return render_template('index.html', actividades=actividades)
 
 if __name__ == "__main__":
     app.run(debug=True)
