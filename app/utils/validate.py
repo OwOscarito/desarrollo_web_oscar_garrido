8f4793ad5bf2b2f7f456bff35889cdee9209159b
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2025-05-15 03:07:39 -0400

    fisrt validators

--- app/utils/validate.py
+++ app/utils/validate.py
@@ -1,45 +1,52 @@
 import re
 import filetype
 
-def validate_username(value):
-    return value and len(value) > 4
+# Generic string validator
+def validate_string(string, min_length=0, max_length=100):
+    if min_length <= len(string) <= max_length:
+        return True
+    return False
+
+# Where
+def validate_location(region, com):
+    if not region or not com:
+        return False
+    
+    return 
+
+def validate_sector(sector):
+    return validate_string(sector, 0, 100)
+
+def validate_name(name):
+    if not name:
+        return False
+    return validate_string(name, 1, 200)
+
+def validate_email(email):
+    EMAIL_REGEX = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+    if not email:
+        return False
+    if not validate_string(email, 1, 100):
+        return False
+    return re.match(EMAIL_REGEX, email)
 
 
-def validate_password(value):
-    malas = ["1234", "admin1", "odio a mis Aux >:(2"]
-    return bool(re.search(r"\d", value)) and not value in malas
 
-
-def validate_email(value):
-    return "@" in value
-
-
-def validate_login_user(username, password):
-    return validate_username(username) and validate_password(password)
-
-
-def validate_register_user(username, password, email):
-    return validate_username(username) and validate_password(password) and validate_email(email)
-
-
-def validate_conf_text(conf_text):
-    return True
-
-
-def validate_conf_img(conf_img):
-    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+def validate_img(img):
+    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "webp"}
     ALLOWED_MIMETYPES = {"image/jpeg", "image/png", "image/gif"}
 
     # check if a file was submitted
-    if conf_img is None:
+    if img is None:
         return False
 
     # check if the browser submitted an empty file
-    if conf_img.filename == "":
+    if img.filename == "":
         return False
     
     # check file extension
-    ftype_guess = filetype.guess(conf_img)
+    ftype_guess = filetype.guess(img)
     if ftype_guess.extension not in ALLOWED_EXTENSIONS:
         return False
     # check mimetype
@@ -48,5 +55,3 @@ def validate_conf_img(conf_img):
     return True
 
 
-def validate_confession(conf_text, conf_img):
-    return validate_conf_text(conf_text) and validate_conf_img(conf_img)
