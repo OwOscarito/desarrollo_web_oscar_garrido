68bc66f5476068f7e0225fdd950af860ac1a460d
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2025-06-13 23:37:40 -0400

    fix page index

--- app/__init__.py
+++ app/__init__.py
@@ -342,7 +342,7 @@ def actividad(id=None):
 
 @app.route("/listado/<int:page>", methods=["GET"])
 @app.route("/listado", methods=["GET"])
-def listado(page=0):
+def listado(page=1):
     activities = []
     for activity in db.get_last_activities(5, page):
         photos = db.get_photos_by_activity_id(activity.id)
