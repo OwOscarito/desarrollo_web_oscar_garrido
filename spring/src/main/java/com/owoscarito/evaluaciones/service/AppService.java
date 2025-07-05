46218f923a3a979828660607ece01fd365f1cc66
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2025-07-04 22:46:30 -0400

    fix format datetime

--- spring/src/main/java/com/owoscarito/evaluaciones/service/AppService.java
+++ spring/src/main/java/com/owoscarito/evaluaciones/service/AppService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
@@ -22,22 +23,25 @@ public class AppService {
     public AppService(ActividadRepositorio actividadRepositorio) {
         this.actividadRepositorio = actividadRepositorio;
     }
-
+    String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy\nHH:mm");
+        return dateTime.format(formatter);
+    }
     public List<Map<String, String>> getDataActividades(Integer pageNum, Integer pageSize) {
         LocalDateTime now = LocalDateTime.now();
         List<Actividad> actividades = actividadRepositorio.findByDiaHoraTerminoLessThanEqual(now, PageRequest.of(pageNum, pageSize)).getContent();
-        System.out.println("---------------Actividades---------------" + actividades);
+        //System.out.println("---------------Actividades---------------" + actividades);
         List<Map<String, String>> dataArray = new ArrayList<>(pageSize);
 
         actividades.forEach( actividad -> {
-            System.out.println("---------------Actividad---------------" + actividad);
+            //System.out.println("---------------Actividad---------------" + actividad);
             Map<String, String> data = new HashMap<>();
 
             data.put("id", actividad.getId().toString());
             data.put("sector", actividad.getSector());
             data.put("nombre", actividad.getNombre());
-            data.put("inicio", actividad.getDiaHoraInicio().toString());
-            data.put("termino", actividad.getDiaHoraTermino() != null ? actividad.getDiaHoraTermino().toString() : "-");
+            data.put("inicio", formatDateTime(actividad.getDiaHoraInicio()));
+            data.put("termino", actividad.getDiaHoraTermino() != null ? formatDateTime(actividad.getDiaHoraTermino()) : "-");
             data.put("temas", actividad.stringTemas());
             data.put("nota", actividad.averageNota());
 
